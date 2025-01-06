460d1b481f05e720a7fd39bbd6f36232742b015c
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-06 12:47:20 +0100

    added serializers

---
+++ market_app/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from market_app.models import Market 
+
+class MarketSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    location = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
+
+    def create(self, validated_data):
+        return Market.create(validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.location = validated_data.get('location', instance.location)
+        instance.description = validated_data.get('description', instance.description)
+        instance.net_worth = validated_data.get('net_wort', instance.net_worth)
+        instance.save()
+        return instance
+
+
+    
